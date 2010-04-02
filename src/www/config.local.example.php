506180b715a9b9825a845a5fb8d0c9d635dfb7bf
Lars Olesen <lars@intraface.dk>
2010-04-02 13:17:39 +0000

    Example config.

---
+++ src/www/config.local.example.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+$GLOBALS['private_key'] = 'JMskFBsyvYykDSjYFPXcTkQ3aZkipv75PCwsxKwpMLzH9qCstu';
+$GLOBALS['template_path'] = realpath(dirname(__FILE__) . '/../Andimo/templates');
+$GLOBALS["cache_dir"] = realpath(dirname(__FILE__));
+
+$debug_log_path = false;
+$debug_enabled = false;
+
+set_include_path('/home/lsolesen/workspace/ilib/IntrafacePublic/IntrafacePublic_CMS/src/' . PATH_SEPARATOR . dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
